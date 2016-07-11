7bf14bcaa8ae14fa09868e9570d9b079f6b52ee4
Viktoras Laukevicius <viktoras.laukevicius@yahoo.com>
2016-07-11 14:38:50 +0300

    Initial Commit

---
+++ Realm-RLMLinkingObjects/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Realm-RLMLinkingObjects
+//
+//  Created by Viktoras Laukevičius on 11/07/16.
+//  Copyright © 2016 Viktoras Laukevičius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Realm-RLMLinkingObjects/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Realm-RLMLinkingObjects
+//
+//  Created by Viktoras Laukevičius on 11/07/16.
+//  Copyright © 2016 Viktoras Laukevičius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
